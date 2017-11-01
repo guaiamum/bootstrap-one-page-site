4ce2b4df3525e5c17c8fe9d4d765731daa21f1bf
Cainã Brazil <guaiamum@Guaiamums-Retina.local>
2017-11-01 00:46:51 -0300

    topzera

--- content/js/smooth-scrolling.js
+++ content/js/smooth-scrolling.js
@@ -1,24 +1,26 @@
 function smoothScrolling() {
   //Smooth automated Scrolling
   var scrollLinks = $('.scroll');
+  var diferencaNavbar = $('#myNavbar').height(); console.log(diferencaNavbar);
 
   scrollLinks.on('click', function(e) {
     e.preventDefault();
-
+    
     $('body,html').animate({
-      scrollTop: $(this.hash).offset().top
+      scrollTop: $(this.hash).offset().top - diferencaNavbar
     }, 1000);
 
   });
 
   //Active link change
   $(window).on('scroll', function() {
-    var scrollbarLocation = $(this).scrollTop(); //height on the top of the web browser
+    var scrollbarLocation = $(this).scrollTop() + diferencaNavbar; //height on the top of the web browser
 
     scrollLinks.each(function() {
-      var sectionOffset = $(this.hash).offset().top - 20;//gets every section height
+      var sectionOffset = $(this.hash).offset().top;//gets every section height
 
-      if (sectionOffset <= scrollbarLocation) { //if height of browser is greater, it is the active link
+      if (sectionOffset < scrollbarLocation + 1) { //if height of browser is greater, it is the active link
+        //console.log(sectionOffset,scrollbarLocation);
         $(this).parent().addClass('active');
         $(this).parent().siblings().removeClass('active');
       }
